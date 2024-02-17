2bd6368b6e6d00e5e3b743c92037f8af8c01751c
Zurihaqi <zurihaqi@gmail.com>
2024-02-17 21:10:05 +0700

    window is undefined on build fix

--- src/hooks/darkThemeHook.js
+++ src/hooks/darkThemeHook.js
@@ -3,8 +3,8 @@ import * as React from "react";
 export default function DarkThemeHook() {
   // Function to detect user's system or browser theme
   const prefersDarkMode =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
+    window?.matchMedia &&
+    window?.matchMedia("(prefers-color-scheme: dark)").matches;
 
   // Set initial theme based on user's system or browser theme
   const [theme, setTheme] = React.useState(
@@ -18,7 +18,7 @@ export default function DarkThemeHook() {
   }, [theme]);
 
   React.useEffect(() => {
-    const root = window.document.documentElement;
+    const root = window?.document.documentElement;
     root.classList.remove(colorTheme);
     root.classList.add(theme);
   }, [theme, colorTheme]);
